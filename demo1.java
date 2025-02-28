39e8a015e7550f477715bd1696e750f0a5c4361a
Ashutosh Kumar pandit <118052705+erashu122@users.noreply.github.com>
2025-02-28 21:02:40 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,22 @@
+// Program to perform arithmetic operations on two numbers
+class demo1 {
+
+    public static void main(String[] args) {
+
+        String n1="13";
+        String n2="3";
+
+        //convert string to integer
+        int a=Integer.parseInt(n1);
+        int b=Integer.parseInt(n2);
+
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));   
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+(a/b));
+        System.out.println("a%b="+(a%b));
+    }
+
+ 
+
+}
---
+++ demo4.java
@@ -0,0 +1,11 @@
+public class demo4 {
+  public static void main(String[] args) {
+    // Logical operators
+    boolean a = true;
+    boolean b = false;
+    System.out.println("a && b: " + (a && b));
+    System.out.println("a || b: " + (a || b));
+    System.out.println("!(a && b): " + !(a && b));
+  }
+    
+}
